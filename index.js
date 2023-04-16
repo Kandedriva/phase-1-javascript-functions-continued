0ec7bd414fa4e42679672debcc73651012dbb1d6
drissakande <drissakande@gmail.com>
2023-04-16 14:33:31 -0400

    Modify index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+let companyName = 'Scuber';
+
+function saturdayFun(fun = "roller-skate" ){
+  return `This Saturday, I want to ${fun}!`;
+}
+
+const mondayWork = function(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(adjective = "*"){
+  return function(wrap = "special"){
+    return `You are ${adjective}${wrap}${adjective}!`
+  };
+}
